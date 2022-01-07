52e140ef857bb9378c8421339ade3eb5cec90532
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-01-07 12:08:37 -0800

    Bring MD5 fully into new endianness paradigm
    This also speeds it up slightly, since the native version doesn't copy
    the input unnecessarily any more.

--- hashes/md5.cpp
+++ hashes/md5.cpp
@@ -49,6 +49,23 @@
 #include "Hashlib.h"
 
 //-----------------------------------------------------------------------------
+// 32-bit integer manipulation functions. These move data in
+// alignment-safe ways, with optional byte swapping.
+template < bool bswap >
+static FORCE_INLINE uint32_t GET_U32(const uint8_t * b, const uint32_t i) {
+    uint32_t n;
+    memcpy(&n, &b[i], 4);
+    n = COND_BSWAP(n, bswap);
+    return n;
+}
+
+template < bool bswap >
+static FORCE_INLINE void PUT_U32(uint32_t n, uint8_t * b, const uint32_t i) {
+    n = COND_BSWAP(n, bswap);
+    memcpy(&b[i], &n, 4);
+}
+
+//-----------------------------------------------------------------------------
 // Raw MD5 implementation
 typedef struct {
     uint32_t total[2];     /*!< number of bytes processed  */
@@ -60,28 +77,9 @@ typedef struct {
 } md5_context;
 
 /*
- * 32-bit integer manipulation macros. These move data in
- * little-endian format no matter the endianness of the system.
- */
-#define GET_U32_LE(n,b,i)                             \
-    {                                                 \
-        (n) = ( (uint32_t) (b)[(i)    ]       )       \
-            | ( (uint32_t) (b)[(i) + 1] <<  8 )       \
-            | ( (uint32_t) (b)[(i) + 2] << 16 )       \
-            | ( (uint32_t) (b)[(i) + 3] << 24 );      \
-    }
-#define PUT_U32_LE(n,b,i)                             \
-    {                                                 \
-        (b)[(i)    ] = (uint8_t) ( (n)       );       \
-        (b)[(i) + 1] = (uint8_t) ( (n) >>  8 );       \
-        (b)[(i) + 2] = (uint8_t) ( (n) >> 16 );       \
-        (b)[(i) + 3] = (uint8_t) ( (n) >> 24 );       \
-    }
-
-/*
  * MD5 context setup
  */
-static void md5_starts(md5_context * ctx) {
+static void md5_start(md5_context * ctx) {
     ctx->total[0] = 0;
     ctx->total[1] = 0;
 
@@ -94,32 +92,33 @@ static void md5_starts(md5_context * ctx) {
 /*
  * MD5 process single data block
  */
+template < bool bswap >
 static void md5_process(md5_context * ctx, uint8_t data[64]) {
     uint32_t X[16], A, B, C, D;
 
-    GET_U32_LE( X[ 0], data,  0 );
-    GET_U32_LE( X[ 1], data,  4 );
-    GET_U32_LE( X[ 2], data,  8 );
-    GET_U32_LE( X[ 3], data, 12 );
-    GET_U32_LE( X[ 4], data, 16 );
-    GET_U32_LE( X[ 5], data, 20 );
-    GET_U32_LE( X[ 6], data, 24 );
-    GET_U32_LE( X[ 7], data, 28 );
-    GET_U32_LE( X[ 8], data, 32 );
-    GET_U32_LE( X[ 9], data, 36 );
-    GET_U32_LE( X[10], data, 40 );
-    GET_U32_LE( X[11], data, 44 );
-    GET_U32_LE( X[12], data, 48 );
-    GET_U32_LE( X[13], data, 52 );
-    GET_U32_LE( X[14], data, 56 );
-    GET_U32_LE( X[15], data, 60 );
+/*
+ * These macros will cache the converted input data when byteswapping
+ * is requested, and will just read directly from data when possible.
+ */
+#define CACHEBLK(k) (bswap ?                        \
+            (X[k]=GET_U32<true>(data, 4*(k))) :     \
+            (GET_U32<false>(data,4*(k))))
+
+#define GETBLK(k) (bswap ?                          \
+            (X[k]) :                                \
+            (GET_U32<false>(data,4*(k))))
 
 #define S(x,n) ((x << n) | ((x & 0xFFFFFFFF) >> (32 - n)))
 
-#define P(a,b,c,d,k,s,t)                                \
-{                                                       \
-    a += F(b,c,d) + X[k] + t; a = S(a,s) + b;           \
-}
+#define P(a,b,c,d,k,s,t)                                   \
+    {                                                      \
+        a += F(b,c,d) + CACHEBLK(k) + t; a = S(a,s) + b;   \
+    }
+
+#define Q(a,b,c,d,k,s,t)                                   \
+    {                                                      \
+        a += F(b,c,d) + GETBLK(k) + t; a = S(a,s) + b;     \
+    }
 
     A = ctx->state[0];
     B = ctx->state[1];
@@ -127,6 +126,7 @@ static void md5_process(md5_context * ctx, uint8_t data[64]) {
     D = ctx->state[3];
 
 #define F(x,y,z) (z ^ (x & (y ^ z)))
+//#define F(x,y,z) ((x & y) | (~x & z))
 
     P( A, B, C, D,  0,  7, 0xD76AA478 );
     P( D, A, B, C,  1, 12, 0xE8C7B756 );
@@ -148,65 +148,66 @@ static void md5_process(md5_context * ctx, uint8_t data[64]) {
 #undef F
 
 #define F(x,y,z) (y ^ (z & (x ^ y)))
+//#define F(x,y,z) ((z & x) | (~z & y))
 
-    P( A, B, C, D,  1,  5, 0xF61E2562 );
-    P( D, A, B, C,  6,  9, 0xC040B340 );
-    P( C, D, A, B, 11, 14, 0x265E5A51 );
-    P( B, C, D, A,  0, 20, 0xE9B6C7AA );
-    P( A, B, C, D,  5,  5, 0xD62F105D );
-    P( D, A, B, C, 10,  9, 0x02441453 );
-    P( C, D, A, B, 15, 14, 0xD8A1E681 );
-    P( B, C, D, A,  4, 20, 0xE7D3FBC8 );
-    P( A, B, C, D,  9,  5, 0x21E1CDE6 );
-    P( D, A, B, C, 14,  9, 0xC33707D6 );
-    P( C, D, A, B,  3, 14, 0xF4D50D87 );
-    P( B, C, D, A,  8, 20, 0x455A14ED );
-    P( A, B, C, D, 13,  5, 0xA9E3E905 );
-    P( D, A, B, C,  2,  9, 0xFCEFA3F8 );
-    P( C, D, A, B,  7, 14, 0x676F02D9 );
-    P( B, C, D, A, 12, 20, 0x8D2A4C8A );
+    Q( A, B, C, D,  1,  5, 0xF61E2562 );
+    Q( D, A, B, C,  6,  9, 0xC040B340 );
+    Q( C, D, A, B, 11, 14, 0x265E5A51 );
+    Q( B, C, D, A,  0, 20, 0xE9B6C7AA );
+    Q( A, B, C, D,  5,  5, 0xD62F105D );
+    Q( D, A, B, C, 10,  9, 0x02441453 );
+    Q( C, D, A, B, 15, 14, 0xD8A1E681 );
+    Q( B, C, D, A,  4, 20, 0xE7D3FBC8 );
+    Q( A, B, C, D,  9,  5, 0x21E1CDE6 );
+    Q( D, A, B, C, 14,  9, 0xC33707D6 );
+    Q( C, D, A, B,  3, 14, 0xF4D50D87 );
+    Q( B, C, D, A,  8, 20, 0x455A14ED );
+    Q( A, B, C, D, 13,  5, 0xA9E3E905 );
+    Q( D, A, B, C,  2,  9, 0xFCEFA3F8 );
+    Q( C, D, A, B,  7, 14, 0x676F02D9 );
+    Q( B, C, D, A, 12, 20, 0x8D2A4C8A );
 
 #undef F
 
 #define F(x,y,z) (x ^ y ^ z)
 
-    P( A, B, C, D,  5,  4, 0xFFFA3942 );
-    P( D, A, B, C,  8, 11, 0x8771F681 );
-    P( C, D, A, B, 11, 16, 0x6D9D6122 );
-    P( B, C, D, A, 14, 23, 0xFDE5380C );
-    P( A, B, C, D,  1,  4, 0xA4BEEA44 );
-    P( D, A, B, C,  4, 11, 0x4BDECFA9 );
-    P( C, D, A, B,  7, 16, 0xF6BB4B60 );
-    P( B, C, D, A, 10, 23, 0xBEBFBC70 );
-    P( A, B, C, D, 13,  4, 0x289B7EC6 );
-    P( D, A, B, C,  0, 11, 0xEAA127FA );
-    P( C, D, A, B,  3, 16, 0xD4EF3085 );
-    P( B, C, D, A,  6, 23, 0x04881D05 );
-    P( A, B, C, D,  9,  4, 0xD9D4D039 );
-    P( D, A, B, C, 12, 11, 0xE6DB99E5 );
-    P( C, D, A, B, 15, 16, 0x1FA27CF8 );
-    P( B, C, D, A,  2, 23, 0xC4AC5665 );
+    Q( A, B, C, D,  5,  4, 0xFFFA3942 );
+    Q( D, A, B, C,  8, 11, 0x8771F681 );
+    Q( C, D, A, B, 11, 16, 0x6D9D6122 );
+    Q( B, C, D, A, 14, 23, 0xFDE5380C );
+    Q( A, B, C, D,  1,  4, 0xA4BEEA44 );
+    Q( D, A, B, C,  4, 11, 0x4BDECFA9 );
+    Q( C, D, A, B,  7, 16, 0xF6BB4B60 );
+    Q( B, C, D, A, 10, 23, 0xBEBFBC70 );
+    Q( A, B, C, D, 13,  4, 0x289B7EC6 );
+    Q( D, A, B, C,  0, 11, 0xEAA127FA );
+    Q( C, D, A, B,  3, 16, 0xD4EF3085 );
+    Q( B, C, D, A,  6, 23, 0x04881D05 );
+    Q( A, B, C, D,  9,  4, 0xD9D4D039 );
+    Q( D, A, B, C, 12, 11, 0xE6DB99E5 );
+    Q( C, D, A, B, 15, 16, 0x1FA27CF8 );
+    Q( B, C, D, A,  2, 23, 0xC4AC5665 );
 
 #undef F
 
 #define F(x,y,z) (y ^ (x | ~z))
 
-    P( A, B, C, D,  0,  6, 0xF4292244 );
-    P( D, A, B, C,  7, 10, 0x432AFF97 );
-    P( C, D, A, B, 14, 15, 0xAB9423A7 );
-    P( B, C, D, A,  5, 21, 0xFC93A039 );
-    P( A, B, C, D, 12,  6, 0x655B59C3 );
-    P( D, A, B, C,  3, 10, 0x8F0CCC92 );
-    P( C, D, A, B, 10, 15, 0xFFEFF47D );
-    P( B, C, D, A,  1, 21, 0x85845DD1 );
-    P( A, B, C, D,  8,  6, 0x6FA87E4F );
-    P( D, A, B, C, 15, 10, 0xFE2CE6E0 );
-    P( C, D, A, B,  6, 15, 0xA3014314 );
-    P( B, C, D, A, 13, 21, 0x4E0811A1 );
-    P( A, B, C, D,  4,  6, 0xF7537E82 );
-    P( D, A, B, C, 11, 10, 0xBD3AF235 );
-    P( C, D, A, B,  2, 15, 0x2AD7D2BB );
-    P( B, C, D, A,  9, 21, 0xEB86D391 );
+    Q( A, B, C, D,  0,  6, 0xF4292244 );
+    Q( D, A, B, C,  7, 10, 0x432AFF97 );
+    Q( C, D, A, B, 14, 15, 0xAB9423A7 );
+    Q( B, C, D, A,  5, 21, 0xFC93A039 );
+    Q( A, B, C, D, 12,  6, 0x655B59C3 );
+    Q( D, A, B, C,  3, 10, 0x8F0CCC92 );
+    Q( C, D, A, B, 10, 15, 0xFFEFF47D );
+    Q( B, C, D, A,  1, 21, 0x85845DD1 );
+    Q( A, B, C, D,  8,  6, 0x6FA87E4F );
+    Q( D, A, B, C, 15, 10, 0xFE2CE6E0 );
+    Q( C, D, A, B,  6, 15, 0xA3014314 );
+    Q( B, C, D, A, 13, 21, 0x4E0811A1 );
+    Q( A, B, C, D,  4,  6, 0xF7537E82 );
+    Q( D, A, B, C, 11, 10, 0xBD3AF235 );
+    Q( C, D, A, B,  2, 15, 0x2AD7D2BB );
+    Q( B, C, D, A,  9, 21, 0xEB86D391 );
 
 #undef F
 
@@ -219,6 +220,7 @@ static void md5_process(md5_context * ctx, uint8_t data[64]) {
 /*
  * MD5 process buffer
  */
+template < bool bswap >
 static void md5_update(md5_context *ctx, uint8_t *input, int ilen) {
     uint32_t fill, left;
 
@@ -234,14 +236,14 @@ static void md5_update(md5_context *ctx, uint8_t *input, int ilen) {
 
     if (left && (ilen >= fill)) {
         memcpy((void *)(ctx->buffer + left), (void *)input, fill);
-        md5_process(ctx, ctx->buffer);
+        md5_process<bswap>(ctx, ctx->buffer);
         input += fill;
         ilen  -= fill;
         left = 0;
     }
 
     while (ilen >= 64) {
-        md5_process(ctx, input);
+        md5_process<bswap>(ctx, input);
         input += 64;
         ilen  -= 64;
     }
@@ -261,6 +263,7 @@ static const uint8_t md5_padding[64] = {
 /*
  * MD5 final digest
  */
+template < bool bswap >
 static void md5_finish(md5_context * ctx, uint8_t output[16]) {
     uint32_t last, padn;
     uint32_t high, low;
@@ -270,19 +273,19 @@ static void md5_finish(md5_context * ctx, uint8_t output[16]) {
          | (ctx->total[1] <<  3);
     low  = (ctx->total[0] <<  3);
 
-    PUT_U32_LE(low,  msglen, 0);
-    PUT_U32_LE(high, msglen, 4);
+    PUT_U32<bswap>(low,  msglen, 0);
+    PUT_U32<bswap>(high, msglen, 4);
 
     last = ctx->total[0] & 0x3F;
     padn = (last < 56) ? (56 - last) : (120 - last);
 
-    md5_update(ctx, (uint8_t *) md5_padding, padn);
-    md5_update(ctx, msglen, 8);
+    md5_update<bswap>(ctx, (uint8_t *) md5_padding, padn);
+    md5_update<bswap>(ctx, msglen, 8);
 
-    PUT_U32_LE(ctx->state[0], output,  0);
-    PUT_U32_LE(ctx->state[1], output,  4);
-    PUT_U32_LE(ctx->state[2], output,  8);
-    PUT_U32_LE(ctx->state[3], output, 12);
+    PUT_U32<bswap>(ctx->state[0], output,  0);
+    PUT_U32<bswap>(ctx->state[1], output,  4);
+    PUT_U32<bswap>(ctx->state[2], output,  8);
+    PUT_U32<bswap>(ctx->state[3], output, 12);
 }
 
 //-----------------------------------------------------------------------------
@@ -300,38 +303,27 @@ static FORCE_INLINE void seed_md5(md5_context * ctx, const seed_t seed) {
 
 //-----------------------------------------------------------------------------
 // Wrappers for rest of SMHasher3
-void MD5_128(const void * in, const size_t len, const seed_t seed, void * out) {
+template < uint32_t hashbits, bool bswap >
+void MD5(const void * in, const size_t len, const seed_t seed, void * out) {
     md5_context md5_ctx;
-    md5_starts(&md5_ctx);
-    seed_md5  (&md5_ctx, seed);
-    md5_update(&md5_ctx, (uint8_t *)in, len);
-    md5_finish(&md5_ctx, (uint8_t *)out);
-}
+    uint8_t buf[16];
+    uint8_t * hash = (hashbits >= 128) ? (uint8_t *)out : &buf[0];
 
-void MD5_64(const void * in, const size_t len, const seed_t seed, void * out) {
-    uint8_t hash[16];
-    md5_context md5_ctx;
-    md5_starts(&md5_ctx);
-    seed_md5  (&md5_ctx, seed);
-    md5_update(&md5_ctx, (uint8_t *)in, len);
-    md5_finish(&md5_ctx, hash);
+    md5_start        (&md5_ctx);
+    seed_md5         (&md5_ctx, seed);
+    md5_update<bswap>(&md5_ctx, (uint8_t *)in, len);
+    md5_finish<bswap>(&md5_ctx, hash);
 
-    // The "B" and "C" states were modified last in the hash rounds,
-    // so return the second and third word of output.
-    memcpy(out, hash + 4, 8);
-}
-
-void MD5_32(const void * in, const size_t len, const seed_t seed, void * out) {
-    uint8_t hash[16];
-    md5_context md5_ctx;
-    md5_starts(&md5_ctx);
-    seed_md5  (&md5_ctx, seed);
-    md5_update(&md5_ctx, (uint8_t *)in, len);
-    md5_finish(&md5_ctx, hash);
-
-    // The "B" state was modified last in the hash round, so return
-    // the second word of output.
-    memcpy(out, hash + 4, 4);
+    // If an abbreviated output is asked for, start with the second
+    // word of the hash, since the "B" state was modified last in the
+    // hash round, followed by "C" in the previous, etc.
+    if (hashbits < 128) {
+        if (hashbits <= 96) {
+            memcpy(out, &hash[4], (hashbits+7)/8);
+        } else {
+            memcpy(out, &hash[0], (hashbits+7)/8);
+        }
+    }
 }
 
 REGISTER_FAMILY(md5);
@@ -343,18 +335,19 @@ REGISTER_HASH(md5_32,
                  FLAG_HASH_ENDIAN_INDEPENDENT   |
                  FLAG_HASH_NO_SEED,
   $.impl_flags = FLAG_IMPL_LICENSE_GPL3         |
+                 FLAG_IMPL_CANONICAL_LE         |
                  FLAG_IMPL_ROTATE               |
                  FLAG_IMPL_VERY_SLOW,
   $.bits = 32,
 #ifdef NOT_YET
   $.verification_LE = 0x4003D7EE,
-  $.verification_BE = 0x4003D7EE,
+  $.verification_BE = 0x53A2E981,
 #else
   $.verification_LE = 0xF3DFF19F,
-  $.verification_BE = 0xF3DFF19F,
+  $.verification_BE = 0x6CC1DCA8,
 #endif
-  $.hashfn_native = MD5_32,
-  $.hashfn_bswap = MD5_32
+  $.hashfn_native = MD5<32,false>,
+  $.hashfn_bswap = MD5<32,true>
 );
 
 REGISTER_HASH(md5_64,
@@ -364,37 +357,39 @@ REGISTER_HASH(md5_64,
                  FLAG_HASH_ENDIAN_INDEPENDENT   |
                  FLAG_HASH_NO_SEED,
   $.impl_flags = FLAG_IMPL_LICENSE_GPL3         |
+                 FLAG_IMPL_CANONICAL_LE         |
                  FLAG_IMPL_ROTATE               |
                  FLAG_IMPL_VERY_SLOW,
   $.bits = 64,
 #ifdef NOT_YET
   $.verification_LE = 0xF2E011D4,
-  $.verification_BE = 0xF2E011D4,
+  $.verification_BE = 0xDE2E1FAD,
 #else
   $.verification_LE = 0x12F0BA8E,
-  $.verification_BE = 0x12F0BA8E,
+  $.verification_BE = 0x9484101A,
 #endif
-  $.hashfn_native = MD5_64,
-  $.hashfn_bswap = MD5_64
+  $.hashfn_native = MD5<64,false>,
+  $.hashfn_bswap = MD5<64,true>
 );
 
-REGISTER_HASH(md5_128,
+REGISTER_HASH(md5,
   $.desc = "MD5",
   $.hash_flags = FLAG_HASH_CRYPTOGRAPHIC        |
                  FLAG_HASH_CRYPTOGRAPHIC_WEAK   |
                  FLAG_HASH_ENDIAN_INDEPENDENT   |
                  FLAG_HASH_NO_SEED,
   $.impl_flags = FLAG_IMPL_LICENSE_GPL3         |
+                 FLAG_IMPL_CANONICAL_LE         |
                  FLAG_IMPL_ROTATE               |
                  FLAG_IMPL_VERY_SLOW,
   $.bits = 128,
 #ifdef NOT_YET
   $.verification_LE = 0x1363415D,
-  $.verification_BE = 0x1363415D,
+  $.verification_BE = 0x242A18E0,
 #else
   $.verification_LE = 0xF263F96F,
-  $.verification_BE = 0xF263F96F,
+  $.verification_BE = 0x5460C3B6,
 #endif
-  $.hashfn_native = MD5_128,
-  $.hashfn_bswap = MD5_128
+  $.hashfn_native = MD5<128,false>,
+  $.hashfn_bswap = MD5<128,true>
 );
