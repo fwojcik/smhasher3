df3cc6084dffefb340424256c2c8be506b013432
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-01-09 21:38:27 -0800

    Rework INSTANTIATE macro so we can drop the C++ standard requirement back down to 11

--- util/Instantiate.h
+++ util/Instantiate.h
@@ -18,13 +18,29 @@
  */
 #include <tuple>
 
+// When this list gets extended, add more ",void*" entries to the
+// C++11 version of INSTANTIATE() below.
 #define HASHTYPELIST uint32_t, uint64_t, Blob<128>, Blob<160>, Blob<224>, Blob<256>
 
+#if defined(__cplusplus) && (__cplusplus >= 201402L)
+// C++14 allows auto variables to determine function return types
 #define INSTANTIATE(FN, TYPELIST)                               \
     template < typename ... Types>                              \
     auto FN ## _instantiator() {                                \
-    static auto instances =                                     \
-        std::tuple_cat(std::make_tuple(FN<Types>)...);          \
-    return &instances;                                          \
+        static auto instances =                                 \
+            std::tuple_cat(std::make_tuple(FN<Types>)...);      \
+        return &instances;                                      \
     }                                                           \
     template auto FN ## _instantiator<TYPELIST>();
+#else
+// C++11 doesn't, so YOU get a void*, and YOU get a void*,....
+#define INSTANTIATE(FN, TYPELIST)                                       \
+    template < typename ... Types>                                      \
+    auto FN ## _instantiator() -> void* {                               \
+        static std::tuple<void*,void*,void*,void*,void*,void*>          \
+            instances = std::tuple_cat(                                 \
+                               std::make_tuple((void*)(FN<Types>))...); \
+        return (void*)(&instances);                                     \
+    }                                                                   \
+    template void* FN ## _instantiator<TYPELIST>();
+#endif
