f20f0ac5f18564bb774d2c84836161ca55982030
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-03-22 12:26:08 -0700

    Bring hasshe2 "up to code"
    Added seeding, added portable implementation, fixed read-past-EOB
    behavior, fixed AppendZeroes, and removed 0-byte special case.
    
    This changes the verification code, so it's a separate commit.

--- hashes/hasshe2.cpp
+++ hashes/hasshe2.cpp
@@ -37,6 +37,7 @@
 #include <immintrin.h>
 #endif
 
+//------------------------------------------------------------
 alignas(16) const static uint32_t coeffs[12] = {
   /* Four carefully selected coefficients and interleaving zeros. */
   0x98b365a1, 0, 0x52c69cab, 0,
@@ -44,6 +45,118 @@ alignas(16) const static uint32_t coeffs[12] = {
   /* 128 bits of random data. */
   0x564a4447, 0xc7265595, 0xe20c241d, 0x128fa608,
 };
+
+//------------------------------------------------------------
+// Portable implementation of the hash
+static void combine_and_mix(uint64_t state[4], const uint64_t input[2]) {
+    /* Phase 1: Perform four 32x32->64 bit multiplication with the
+       input block and words 1 and 3 coeffs, respectively.  This
+       effectively propagates a bit change in input to 32 more
+       significant bit positions.  Combine into internal state by
+       subtracting the result of multiplications from the internal
+       state. */
+    state[0] -= ((uint64_t)(coeffs[0])) * (input[1] & 0xffffffff);
+    state[1] -= ((uint64_t)(coeffs[2])) * (input[1] >> 32);
+    state[2] -= ((uint64_t)(coeffs[4])) * (input[0] & 0xffffffff);
+    state[3] -= ((uint64_t)(coeffs[6])) * (input[0] >> 32);
+
+    /* Phase 2: Perform shifts and xors to propagate the 32-bit
+       changes produced above into 64-bit (and even a little larger)
+       changes in the internal state. */
+    /* state ^= state >64> 29; */
+    /* state +64= state <64< 16; */
+    /* state ^= state >64> 21; */
+    for (int i = 0; i < 4; i++) {
+        state[i] ^= state[i] >> 29;
+        state[i] += state[i] << 16;
+        state[i] ^= state[i] >> 21;
+    }
+    state[1] += (state[1] << 32) + (state[0] >> 32);
+    state[0] += (state[0] << 32);
+    state[3] += (state[3] << 32) + (state[2] >> 32);
+    state[2] += (state[2] << 32);
+
+    /* Phase 3: Propagate the changes among the four 64-bit words by
+       performing 64-bit subtractions and 32-bit word shuffling. */
+    state[0] -= state[2];
+    state[1] -= state[3];
+
+    uint64_t tmp;
+
+    tmp = state[2];
+    state[2] = ((state[2] >> 32) + (state[3] << 32)) - state[0];
+    state[3] = ((state[3] >> 32) + (tmp      << 32)) - state[1];
+
+    tmp = state[1];
+    state[1] = ((state[0] >> 32) + (state[0] << 32)) - state[3];
+    state[0] = tmp - state[2];
+
+    tmp = state[2];
+    state[2] = ((state[3] >> 32) + (state[2] << 32)) - state[0];
+    state[3] = ((tmp      >> 32) + (state[3] << 32)) - state[1];
+
+    tmp = state[0];
+    state[0] = ((state[1] >> 32) + (state[0] << 32)) - state[2];
+    state[1] = ((tmp      >> 32) + (state[1] << 32)) - state[3];
+
+    /* With good coefficients any one-bit flip in the input has now
+       changed all bits in the internal state with a probability
+       between 45% to 55%. */
+}
+
+static void hasshe2_portable(const uint8_t * input_buf, size_t n_bytes, uint64_t seed, void *output_state) {
+    uint64_t state[4];
+    uint64_t input[2];
+    uint64_t seed2 = seed + (uint64_t)n_bytes;
+
+    /* Initialize internal state to something random.  (Alternatively,
+       if hashing a chain of data, read in the previous hash result from
+       somewhere.) */
+    state[0] = coeffs[ 8] + (((uint64_t)coeffs[ 9]) << 32);
+    state[1] = coeffs[10] + (((uint64_t)coeffs[11]) << 32);
+    state[0] ^= seed;
+    state[1] ^= (seed + (uint64_t)n_bytes);
+    state[2] = state[0];
+    state[3] = state[1];
+
+    while (n_bytes >= 16) {
+        /* Read in 16 bytes, or 128 bits, from buf.  Advance buf and
+           decrement n_bytes accordingly. */
+        for (int i = 0; i < 2; i++) {
+            input[i] = GET_U64<false>(input_buf, i*8);
+        }
+        input_buf += 16;
+        n_bytes -= 16;
+
+        combine_and_mix(state, input);
+    }
+    if (n_bytes > 0) {
+        uint8_t buf[16];
+        memcpy(buf, input_buf, n_bytes);
+        memset(buf + n_bytes, 0, 16 - n_bytes);
+        for (int i = 0; i < 2; i++) {
+            input[i] = GET_U64<false>(buf, i*8);
+        }
+
+        combine_and_mix(state, input);
+    }
+
+    /* Postprocessing.  Copy half of the internal state into fake input,
+       replace it with the constant rnd_data, and do one combine and mix
+       phase more. */
+    input[0] = state[0];
+    input[1] = state[1];
+    state[0] = coeffs[ 8] + (((uint64_t)coeffs[ 9]) << 32);
+    state[1] = coeffs[10] + (((uint64_t)coeffs[11]) << 32);
+    combine_and_mix(state, input);
+
+    for (int i = 0; i < 4; i++) {
+        PUT_U64<false>(state[i], (uint8_t *)output_state, i*8);
+    }
+}
+
+//------------------------------------------------------------
+#if defined(NEW_HAVE_SSE_2)
 
 #define COMBINE_AND_MIX(c_1, c_2, s_1, s_2, in)                              \
   /* Phase 1: Perform four 32x32->64 bit multiplication with the             \
@@ -54,7 +167,6 @@ alignas(16) const static uint32_t coeffs[12] = {
      state. */                                                               \
   s_1 = _mm_sub_epi64(s_1, _mm_mul_epu32(c_1, _mm_unpackhi_epi32(in, in)));  \
   s_2 = _mm_sub_epi64(s_2, _mm_mul_epu32(c_2, _mm_unpacklo_epi32(in, in)));  \
-                                                                             \
   /* Phase 2: Perform shifts and xors to propagate the 32-bit                \
      changes produced above into 64-bit (and even a little larger)           \
      changes in the internal state. */                                       \
@@ -83,30 +195,18 @@ alignas(16) const static uint32_t coeffs[12] = {
      changed all bits in the internal state with a probability               \
      between 45% to 55%. */
 
-static void hasshe2(const uint8_t * input_buf, size_t n_bytes, uint64_t seed, void *output_state) {
+static void hasshe2_sse2(const uint8_t * input_buf, size_t n_bytes, uint64_t seed, void *output_state) {
   __m128i coeffs_1, coeffs_2, rnd_data, seed_xmm, input, state_1, state_2;
 
-#if 0
-#else  
-  if (n_bytes % 16) {
-      //add pad NUL
-      n_bytes += 16 - (n_bytes % 16);
-  }
-#endif  
-  
   coeffs_1 = _mm_load_si128((__m128i *) coeffs);
   coeffs_2 = _mm_load_si128((__m128i *) (coeffs + 4));
   rnd_data = _mm_load_si128((__m128i *) (coeffs + 8));
-  seed_xmm = _mm_set1_epi64x(seed);
+  seed_xmm = _mm_set_epi64x(seed + n_bytes, seed);
 
   /* Initialize internal state to something random.  (Alternatively,
      if hashing a chain of data, read in the previous hash result from
      somewhere.) */
-#if 0
   state_1 = state_2 = _mm_xor_si128(rnd_data, seed_xmm);
-#else
-  state_1 = state_2 = rnd_data;
-#endif
 
   while (n_bytes >= 16) {
       /* Read in 16 bytes, or 128 bits, from buf.  Advance buf and
@@ -117,33 +217,33 @@ static void hasshe2(const uint8_t * input_buf, size_t n_bytes, uint64_t seed, vo
 
       COMBINE_AND_MIX(coeffs_1, coeffs_2, state_1, state_2, input);
   }
-#if 0
   if (n_bytes > 0) {
-      uint8_t buf[16];
+      alignas(16) uint8_t buf[16];
       memcpy(buf, input_buf, n_bytes);
       memset(buf + n_bytes, 0, 16 - n_bytes);
-      input = _mm_loadu_si128((__m128i *) buf);
+      input = _mm_load_si128((__m128i *) buf);
       COMBINE_AND_MIX(coeffs_1, coeffs_2, state_1, state_2, input);
   }
-#endif
 
   /* Postprocessing.  Copy half of the internal state into fake input,
      replace it with the constant rnd_data, and do one combine and mix
      phase more. */
   input = state_1;
   state_1 = rnd_data;
+
   COMBINE_AND_MIX(coeffs_1, coeffs_2, state_1, state_2, input);
 
   _mm_storeu_si128((__m128i *)output_state, state_1);
   _mm_storeu_si128((__m128i *)((char*)output_state + 16), state_2);
 }
+#endif
 
 void Hasshe2(const void * in, const size_t len, const seed_t seed, void * out) {
-    if (len == 0) {
-        PUT_U32<false>(0, (uint8_t *)out, 0);
-    } else {
-        hasshe2((const uint8_t *)in, len, (uint64_t)seed, out);
-    }
+#if defined(NEW_HAVE_SSE_2)
+    hasshe2_sse2((const uint8_t *)in, len, (uint64_t)seed, out);
+#else
+    hasshe2_portable((const uint8_t *)in, len, (uint64_t)seed, out);
+#endif
 }
 
 REGISTER_FAMILY(hasshe2);
@@ -151,11 +251,13 @@ REGISTER_FAMILY(hasshe2);
 REGISTER_HASH(hasshe2,
   $.desc = "hasshe2 (SSE2-oriented hash)",
   $.hash_flags =
-        0,
+        FLAG_HASH_NO_SEED,
   $.impl_flags =
+        FLAG_IMPL_64BIT                  |
+        FLAG_IMPL_MULTIPLY               |
         FLAG_IMPL_LICENSE_PUBLIC_DOMAIN,
   $.bits = 256,
-  $.verification_LE = 0xF5D39DFE,
+  $.verification_LE = 0xBAF6B1BF,
   $.verification_BE = 0x0,
   $.hashfn_native = Hasshe2,
   $.hashfn_bswap = Hasshe2
