7c1a93a647ba463246f1b2e980b6715aa42dd42e
Frank J. T. Wojcik <fwojcik@uw.edu>
2022-08-26 14:09:46 -0700

    Fix formatting in khashv.cpp (NFC)
    This reformats the file with uncrustify, removes the platform-specific quote
    characters, and makes the license look the same as all the other MIT-licensed files
    in SMHasher3 (text is unchanged).

--- hashes/khashv.cpp
+++ hashes/khashv.cpp
@@ -1,22 +1,29 @@
 /*
-MIT License
-Copyright (c) 2022 Keith-Cancel
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the “Software”), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ * khashv
+ * Copyright (c) 2022 Keith-Cancel
+ * Copyright (C) 2022 Frank J. T. Wojcik
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 #include "Platform.h"
 #include "Hashlib.h"
 #include "Intrinsics.h"
@@ -27,24 +34,26 @@ SOFTWARE.
 static thread_local khashvSeed khashv_32_seed;
 static thread_local khashvSeed khashv_64_seed;
 
-static uintptr_t khashv32_init_seed(const seed_t seed ) {
+static uintptr_t khashv32_init_seed( const seed_t seed ) {
     khashv_prep_seed64(&khashv_32_seed, (uint64_t)seed);
     return (uintptr_t)(&khashv_32_seed);
 }
 
-static uintptr_t khashv64_init_seed(const seed_t seed ) {
+static uintptr_t khashv64_init_seed( const seed_t seed ) {
     khashv_prep_seed64(&khashv_64_seed, (uint64_t)seed);
     return (uintptr_t)(&khashv_64_seed);
 }
 
-static void khashv32_test(const void * in, const size_t len, const seed_t seed, void * out ) {
-    uint32_t hash = khashv32((khashvSeed*)(uintptr_t)seed, (const uint8_t*)in, len);
+static void khashv32_test( const void * in, const size_t len, const seed_t seed, void * out ) {
+    uint32_t hash = khashv32((khashvSeed *)(uintptr_t)seed, (const uint8_t *)in, len);
+
     hash = COND_BSWAP(hash, isBE());
     PUT_U32<false>(hash, (uint8_t *)out, 0);
 }
 
-static void khashv64_test(const void * in, const size_t len, const seed_t seed, void * out ) {
-    uint64_t hash = khashv64((khashvSeed*)(uintptr_t)seed, (const uint8_t*)in, len);
+static void khashv64_test( const void * in, const size_t len, const seed_t seed, void * out ) {
+    uint64_t hash = khashv64((khashvSeed *)(uintptr_t)seed, (const uint8_t *)in, len);
+
     hash = COND_BSWAP(hash, isBE());
     PUT_U64<false>(hash, (uint8_t *)out, 0);
 }
@@ -59,8 +68,8 @@ REGISTER_HASH(khashv_32,
    $.hash_flags =
         FLAG_HASH_ENDIAN_INDEPENDENT,
    $.impl_flags =
-        FLAG_IMPL_CANONICAL_BOTH |
-        FLAG_IMPL_LICENSE_MIT,
+        FLAG_IMPL_CANONICAL_BOTH    |
+        FLAG_IMPL_LICENSE_MIT       ,
    $.bits = 32,
    $.verification_LE = 0xB69DF8EB,
    $.verification_BE = 0xB69DF8EB,
@@ -74,8 +83,8 @@ REGISTER_HASH(khashv_64,
     $.hash_flags =
             FLAG_HASH_ENDIAN_INDEPENDENT,
     $.impl_flags =
-            FLAG_IMPL_CANONICAL_BOTH |
-            FLAG_IMPL_LICENSE_MIT,
+            FLAG_IMPL_CANONICAL_BOTH    |
+            FLAG_IMPL_LICENSE_MIT       ,
     $.bits = 64,
     $.verification_LE = 0xA6B7E55B,
     // Should be the same on BE systems. It just won't be vectorized with GCCs
