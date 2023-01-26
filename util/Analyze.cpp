da8810a941074e126c924c92b9d015e377759126
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-01-25 22:15:42 -0800

    NFC: Make some variable names be more consistent

--- util/Analyze.cpp
+++ util/Analyze.cpp
@@ -1074,12 +1074,12 @@ bool ReportChiSqIndep( const uint32_t * popcount, const uint32_t * andcount, siz
 
     const double p_value_raw = ChiSqPValue(maxChiSq, 1);
     const double p_value     = ScalePValue(p_value_raw, keybits * realhashbitpairs);
-    const int    log2_pvalue = GetLog2PValue(p_value);
+    const int    logp_value  = GetLog2PValue(p_value);
     const double cramer_v    = sqrt(maxChiSq / testcount);
 
-    recordLog2PValue(log2_pvalue);
+    recordLog2PValue(logp_value);
     printf("max %6.4f at bit %4d -> out (%3d,%3d)  (^%2d)", cramer_v,
-            maxKeybit, maxOutbitA, maxOutbitB, log2_pvalue);
+            maxKeybit, maxOutbitA, maxOutbitB, logp_value);
 
     if (p_value < FAILURE_PBOUND) {
         printf(" !!!!!\n");
