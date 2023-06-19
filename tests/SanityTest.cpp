6226462a80768678e837cb75f4df4e6b8a86f717
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-06-19 14:59:33 -0700

    Fix printf() in SanityTest

--- tests/SanityTest.cpp
+++ tests/SanityTest.cpp
@@ -337,7 +337,7 @@ bool SanityTest2( const HashInfo * hinfo, bool verbose ) {
                         *ptr ^= 0xFF;
                         hash(key2, len, seed, hash3);
                         if (memcmp(hash2, hash3, hashbytes) != 0) {
-                            maybeprintf(" changing single non-key byte (%s%d) altered hash: ",
+                            maybeprintf(" changing single non-key byte (%s%zd) altered hash: ",
                                     ptr < key2_start ? "head -" : "tail +",
                                     ptr < key2_start ? key2_start - ptr : ptr - key2_end + 1);
                             result = false;
