f2f6c8500562c13e2aa40620ba0176ca9659f203
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-07-14 19:03:19 -0700

    timing_variant1: Simplify asm constraints
    As in the previous commit for variant2, when doing "cpuid; rdtsc" there's
    no need to copy the outputs; just tell the compiler to expect them in %eax
    & %edx. When doing "rdtsc; cpuid", %eax and %edx must be moved, but they
    may be moved to memory locations ("=g" constraint). Also eliminated
    unneeded trailing \n\t from asm strings.

--- platform/timing_variant1.h
+++ platform/timing_variant1.h
@@ -2,11 +2,9 @@ FORCE_INLINE uint64_t timer_start() {
     uint64_t cycles_high, cycles_low;
     __asm__ volatile
         ("cpuid\n\t"
-         "rdtsc\n\t"
-         "mov %%rdx, %0\n\t"
-         "mov %%rax, %1\n\t" :
-         "=r" (cycles_high), "=r" (cycles_low) ::
-         "%rax", "%rbx", "%rcx", "%rdx");
+         "rdtsc" :
+         "=d" (cycles_high), "=a" (cycles_low) ::
+         "%rbx", "%rcx");
     return (cycles_high << 32) | cycles_low;
 }
 
@@ -16,8 +14,8 @@ FORCE_INLINE uint64_t timer_end() {
         ("rdtscp\n\t"
          "mov %%rdx, %0\n\t"
          "mov %%rax, %1\n\t"
-         "cpuid\n\t" :
-         "=r" (cycles_high), "=r" (cycles_low) ::
+         "cpuid" :
+         "=g" (cycles_high), "=g" (cycles_low) ::
          "%rax", "%rbx", "%rcx", "%rdx");
     return (cycles_high << 32) | cycles_low;
 }
