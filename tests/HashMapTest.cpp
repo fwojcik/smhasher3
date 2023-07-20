fd6f01ffc7385daeacca2d1e00fb7ccc8928f43f
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-07-20 12:12:27 -0700

    Update copyrights (NFC)

--- tests/HashMapTest.cpp
+++ tests/HashMapTest.cpp
@@ -1,6 +1,7 @@
 /*
  * SMHasher3
- * Copyright (C) 2021-2022  Frank J. T. Wojcik
+ * Copyright (C) 2021-2023  Frank J. T. Wojcik
+ * Copyright (C) 2023       jason
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
