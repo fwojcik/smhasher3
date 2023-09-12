45557df3db2af284acbb2a137620b53ff999b4d0
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-09-12 12:01:50 -0700

    Update pengyhash to v0.3 (commit 9b70a18)

--- hashes/pengyhash.cpp
+++ hashes/pengyhash.cpp
@@ -1,72 +1,69 @@
 /*
- * Pengyhash, v0.2
+ * Pengyhash, v0.3
  * Copyright (C) 2021-2023  Frank J. T. Wojcik
+ * Copyright (c) 2023       Alberto Fajardo
  * Copyright (C) 2023       jason
- * Copyright (c) 2020 Alberto Fajardo
- * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
- * * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the
- * distribution.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "Platform.h"
 #include "Hashlib.h"
 
 //------------------------------------------------------------
-template <bool bswap>
+#define V64(p) (                                                                                               \
+                (uint64_t)(p)[0]     | (uint64_t)(p)[1]<<8  | (uint64_t)(p)[2]<<16 | (uint64_t)(p)[3]<<24 |    \
+                (uint64_t)(p)[4]<<32 | (uint64_t)(p)[5]<<40 | (uint64_t)(p)[6]<<48 | (uint64_t)(p)[7]<<56      \
+)
+
 static uint64_t pengyhash( const uint8_t * p, size_t size, uint64_t seed ) {
-    uint64_t b[4] = { 0 };
-    uint64_t s[4] = { 0, 0, 0, size };
-    int      i;
+    uint64_t s[4] = { 0 };
+    uint64_t f[4] = { 0 };
+    uint8_t  i;
 
-    for (; size >= 32; size -= 32, p += 32) {
-        memcpy(b, p, 32);
-
-        s[1] = (s[0] += s[1] + GET_U64<bswap>((uint8_t *)&b[3], 0)) + ROTL64(s[1], 14);
-        s[3] = (s[2] += s[3] + GET_U64<bswap>((uint8_t *)&b[2], 0)) + ROTL64(s[3], 23);
-        s[3] = (s[0] += s[3] + GET_U64<bswap>((uint8_t *)&b[1], 0)) ^ ROTL64(s[3], 16);
-        s[1] = (s[2] += s[1] + GET_U64<bswap>((uint8_t *)&b[0], 0)) ^ ROTL64(s[1], 40);
+    for (*s = size; size >= 32; size -= 32, p += 32) {
+        s[1] += V64(p +  8); s[1] = (s[0] += s[1] + V64(p)     ) ^ ROTL64(s[1], 14);
+        s[3] += V64(p + 24); s[3] = (s[2] += s[3] + V64(p + 16)) ^ ROTL64(s[3], 23);
+        s[3] += V64(p + 24); s[3] = (s[0] += s[3] + V64(p)     ) ^ ROTL64(s[3], 11);
+        s[1] += V64(p +  8); s[1] = (s[2] += s[1] + V64(p + 16)) ^ ROTL64(s[1], 40);
     }
 
-    memcpy(b, p, size);
+    for (i = 0; (size_t)(i + 8) < size; i += 8, p += 8) {
+        f[i / 8] = V64(p);
+    }
+    for (; i < size; i++) {
+        f[i / 8] |= (uint64_t)p[i % 8] << i % 8 * 8;
+    }
 
     for (i = 0; i < 6; i++) {
-        s[1] = (s[0] += s[1] + GET_U64<bswap>((uint8_t *)&b[3], 0)) + ROTL64(s[1], 14) + seed;
-        s[3] = (s[2] += s[3] + GET_U64<bswap>((uint8_t *)&b[2], 0)) + ROTL64(s[3], 23);
-        s[3] = (s[0] += s[3] + GET_U64<bswap>((uint8_t *)&b[1], 0)) ^ ROTL64(s[3], 16);
-        s[1] = (s[2] += s[1] + GET_U64<bswap>((uint8_t *)&b[0], 0)) ^ ROTL64(s[1], 40);
+        s[1] += seed;
+        s[1] += f[1]; s[1] = (s[0] += s[1] + f[0]) ^ ROTL64(s[1], 14);
+        s[3] += f[3]; s[3] = (s[2] += s[3] + f[2]) ^ ROTL64(s[3], 23);
+        s[3] += f[3]; s[3] = (s[0] += s[3] + f[0]) ^ ROTL64(s[3],  9);
+        s[1] += f[1]; s[1] = (s[2] += s[1] + f[2]) ^ ROTL64(s[1], 40);
     }
 
     return s[0] + s[1] + s[2] + s[3];
 }
 
 //------------------------------------------------------------
-template <bool bswap>
 static void pengy( const void * in, const size_t len, const seed_t seed, void * out ) {
-    uint64_t h = pengyhash<bswap>((const uint8_t *)in, len, (uint64_t)seed);
+    uint64_t h = pengyhash((const uint8_t *)in, len, (uint64_t)seed);
 
-    PUT_U64<bswap>(h, (uint8_t *)out, 0);
+    h = COND_BSWAP(h, isBE());
+    PUT_U64<false>(h, (uint8_t *)out, 0);
 }
 
 //------------------------------------------------------------
@@ -76,15 +73,16 @@ REGISTER_FAMILY(pengyhash,
  );
 
 REGISTER_HASH(pengyhash,
-   $.desc       = "pengyhash v0.2",
+   $.desc       = "pengyhash v0.3",
    $.hash_flags =
-         0,
+        FLAG_HASH_ENDIAN_INDEPENDENT,
    $.impl_flags =
-         FLAG_IMPL_ROTATE       |
-         FLAG_IMPL_LICENSE_BSD,
+        FLAG_IMPL_ROTATE            |
+        FLAG_IMPL_CANONICAL_BOTH    |
+        FLAG_IMPL_LICENSE_GPL3,
    $.bits = 64,
-   $.verification_LE = 0x1FC2217B,
-   $.verification_BE = 0x774D23AB,
-   $.hashfn_native   = pengy<false>,
-   $.hashfn_bswap    = pengy<true>
+   $.verification_LE = 0x861A1254,
+   $.verification_BE = 0x861A1254,
+   $.hashfn_native   = pengy,
+   $.hashfn_bswap    = pengy
  );
