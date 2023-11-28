52dbc36d4937507a6f154209670e041288ee478e
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-11-28 15:51:10 -0800

    Suppress some gcc warnings (NFC)

--- platform/fixedint128_variant1.h
+++ platform/fixedint128_variant1.h
@@ -1,3 +1,6 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
 typedef unsigned __int128 uint128_t;
 typedef          __int128  int128_t;
+#pragma GCC diagnostic pop
 #define HAVE_INT128
