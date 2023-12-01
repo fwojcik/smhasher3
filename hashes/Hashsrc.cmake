23c7d81066a8ded45f8570390d79ebf03145a1f7
Frank J. T. Wojcik <fwojcik@uw.edu>
2023-12-01 14:29:00 -0800

    Reorder list of hash source files by .o size

--- hashes/Hashsrc.cmake
+++ hashes/Hashsrc.cmake
@@ -9,72 +9,75 @@ set(HASH_SRC_FILES
 #########################################
 #
 #  hashes/pmp_multilinear.cpp
-  hashes/xxhash.cpp
-  hashes/t1ha.cpp
   hashes/farmhash.cpp
-  hashes/halftimehash.cpp
-  hashes/blake2.cpp
-  hashes/blake3.cpp
-  hashes/umash.cpp
   hashes/mum_mir.cpp
+  hashes/halftimehash.cpp
+  hashes/blake3.cpp
+  hashes/xxhash.cpp
+  hashes/blake2.cpp
+  hashes/umash.cpp
   hashes/cityhash.cpp
+  hashes/rmd.cpp
+  hashes/t1ha.cpp
+  hashes/abseil.cpp
+  hashes/metrohash.cpp
+  hashes/rust-ahash.cpp
+  hashes/highwayhash.cpp
+  hashes/clhash.cpp
+  hashes/discohash.cpp
+  hashes/chaskey.cpp
+  hashes/siphash.cpp
   hashes/nmhash.cpp
   hashes/ascon.cpp
-  hashes/rmd.cpp
-  hashes/beamsplitter.cpp
-  hashes/sha2.cpp
-  hashes/sha1.cpp
-  hashes/pearson.cpp
-  hashes/vmac.cpp
-  hashes/clhash.cpp
-  hashes/spookyhash.cpp
-  hashes/meowhash.cpp
-  hashes/metrohash.cpp
-  hashes/farsh.cpp
-  hashes/siphash.cpp
-  hashes/crc.cpp
-  hashes/chaskey.cpp
-  hashes/tabulation.cpp
-  hashes/komihash.cpp
-  hashes/multiply_shift.cpp
-  hashes/md5.cpp
-  hashes/fnv.cpp
-  hashes/hasshe2.cpp
   hashes/prvhash.cpp
-  hashes/murmurhash3.cpp
-  hashes/wyhash.cpp
-  hashes/discohash.cpp
-  hashes/sha3.cpp
+  hashes/vmac.cpp
+  hashes/aesrng.cpp
+  hashes/meowhash.cpp
+  hashes/spookyhash.cpp
+  hashes/sha2.cpp
+  hashes/rainstorm.cpp
+  hashes/sha1.cpp
+  hashes/farsh.cpp
+  hashes/hasshe2.cpp
+  hashes/perlhashes.cpp
+  hashes/beamsplitter.cpp
+  hashes/khashv.cpp
+  hashes/polymur.cpp
+  hashes/jodyhash.cpp
   hashes/poly_mersenne.cpp
-  hashes/murmurhash2.cpp
   hashes/falkhash.cpp
+  hashes/fnv.cpp
+  hashes/rainbow.cpp
+  hashes/wyhash.cpp
   hashes/aquahash.cpp
+  hashes/aesnihash-peterrk.cpp
+  hashes/md5.cpp
+  hashes/murmurhash3.cpp
+  hashes/mx3.cpp
+  hashes/crc.cpp
+  hashes/pearson.cpp
+  hashes/multiply_shift.cpp
+  hashes/komihash.cpp
+  hashes/aesnihash-majek.cpp
+  hashes/tabulation.cpp
+  hashes/khash.cpp
+  hashes/murmurhash2.cpp
   hashes/fletcher.cpp
+  hashes/floppsyhash.cpp
+  hashes/badhash.cpp
+  hashes/seahash.cpp
   hashes/crap.cpp
   hashes/blockpearson.cpp
-  hashes/perlhashes.cpp
-  hashes/jodyhash.cpp
-  hashes/khash.cpp
-  hashes/floppsyhash.cpp
-  hashes/seahash.cpp
-  hashes/rainstorm.cpp
-  hashes/rainbow.cpp
-  hashes/falcon_oaat.cpp
+  hashes/sha3.cpp
   hashes/lookup3.cpp
-  hashes/fasthash.cpp
-  hashes/aesnihash-peterrk.cpp
-  hashes/aesnihash-majek.cpp
-  hashes/o1hash.cpp
-  hashes/pengyhash.cpp
-  hashes/mx3.cpp
-  hashes/superfasthash.cpp
-  hashes/khashv.cpp
   hashes/murmurhash1.cpp
+  hashes/donothing.cpp
+  hashes/rust-fxhash.cpp
+  hashes/falcon_oaat.cpp
+  hashes/fasthash.cpp
+  hashes/pengyhash.cpp
+  hashes/superfasthash.cpp
+  hashes/o1hash.cpp
   hashes/murmur_oaat.cpp
   hashes/x17.cpp
-  hashes/highwayhash.cpp
-  hashes/abseil.cpp
-  hashes/polymur.cpp
-  hashes/rust-fxhash.cpp
-  hashes/rust-ahash.cpp
 )
