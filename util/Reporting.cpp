28d2599dc817f552fbc4c4493834f1b2a740b8a2
Frank J. T. Wojcik <fwojcik@uw.edu>
2024-02-13 10:50:38 -0800

    Tweak failure bounds to be more like p=0.01 than p=0.05

--- util/Reporting.cpp
+++ util/Reporting.cpp
@@ -63,9 +63,9 @@
 
 //-----------------------------------------------------------------------------
 // If score exceeds this improbability of happening, note a failing result
-static const double FAILURE_PBOUND = exp2(-18); // 2**-18 == 1/262,144 =~ 0.000381%
+static const double FAILURE_PBOUND = exp2(-20); // 2**-20 == 1/1,048,576 =~ 0.00009537%
 // If score exceeds this improbability of happening, note a warning
-static const double WARNING_PBOUND = exp2(-15); // 2**-15 == 1/32,768  =~ 0.00305%, 8x as much as failure
+static const double WARNING_PBOUND = exp2(-16); // 2**-16 == 1/65,536  =~ 0.001526%, 16x as much as failure
 // If these bounds seem overly generous, remember that SMHasher3 uses
 // about 16,000 tests, so a 1/16,000 chance event will hit once per run on
 // average, even with a perfect-quality hash function.
