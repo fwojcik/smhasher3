f3c710c6c78f4727d860ae05d3e4429bc449b5c1
Frank J. T. Wojcik <fwojcik@uw.edu>
2024-12-22 13:08:43 -0800

    Minor touchups for ChibiHash
      - Remove system header includes, use std::ptrdiff_t instead
      - Use the usual SMHasher3 naming conventions
      - Code formatting changes

--- hashes/chibihash.cpp
+++ hashes/chibihash.cpp
@@ -8,22 +8,19 @@
 #include "Platform.h"
 #include "Hashlib.h"
 
-#include <stdint.h>
-#include <stddef.h>
-
 #define chibihash64__load64le(p)   GET_U64<bswap>(p, 0)
 #define chibihash64__load32le(p)   ((uint64_t)GET_U32<bswap>(p, 0))
 #define chibihash64__rotl(x, n)    ROTL64(x, n)
 
 template <bool bswap>
-static void ChibiHash64_V2( const void * in, const size_t len, const seed_t seedIn, void * out) {
-    uint64_t seed = (uint64_t)seedIn;
-    const uint8_t *p = (const uint8_t *)in;
-    ptrdiff_t l = len;
+static void ChibiHash64_V2( const void * in, const size_t len, const seed_t seedIn, void * out ) {
+    uint64_t        seed = (uint64_t)seedIn;
+    const uint8_t * p    = (const uint8_t *)in;
+    std::ptrdiff_t  l    = len;
 
-    const uint64_t K = UINT64_C(0x2B7E151628AED2A7); // digits of e
-    uint64_t seed2 = chibihash64__rotl(seed-K, 15) + chibihash64__rotl(seed-K, 47);
-    uint64_t h[4] = { seed, seed+K, seed2, seed2+(K*K^K) };
+    const uint64_t K     = UINT64_C(0x2B7E151628AED2A7); // digits of e
+    uint64_t       seed2 = chibihash64__rotl(seed - K, 15) + chibihash64__rotl(seed - K, 47);
+    uint64_t       h[4]  = { seed, seed + K, seed2, seed2 + (K * K ^ K) };
 
     // depending on your system unrolling might (or might not) make things
     // a tad bit faster on large strings. on my system, it actually makes
@@ -32,26 +29,27 @@ static void ChibiHash64_V2( const void * in, const size_t len, const seed_t seed
     // worth the trade-off since larger code-size will hinder inlinability
     // but depending on your needs, you may want to uncomment the pragma
     // below to unroll the loop.
+    //
     //#pragma GCC unroll 2
     for (; l >= 32; l -= 32) {
         for (int i = 0; i < 4; ++i, p += 8) {
             uint64_t stripe = chibihash64__load64le(p);
             h[i] = (stripe + h[i]) * K;
-            h[(i+1)&3] += chibihash64__rotl(stripe, 27);
+            h[(i + 1) & 3] += chibihash64__rotl(stripe, 27);
         }
     }
 
     for (; l >= 8; l -= 8, p += 8) {
-        h[0] ^= chibihash64__load32le(p+0); h[0] *= K;
-        h[1] ^= chibihash64__load32le(p+4); h[1] *= K;
+        h[0] ^= chibihash64__load32le(p + 0); h[0] *= K;
+        h[1] ^= chibihash64__load32le(p + 4); h[1] *= K;
     }
 
     if (l >= 4) {
-        h[2] ^= chibihash64__load32le(p);
+        h[2] ^= chibihash64__load32le(p        );
         h[3] ^= chibihash64__load32le(p + l - 4);
     } else if (l > 0) {
         h[2] ^= p[0];
-        h[3] ^= p[l/2] | ((uint64_t)p[l-1] << 8);
+        h[3] ^= p[l / 2] | ((uint64_t)p[l - 1] << 8);
     }
 
     h[0] += chibihash64__rotl(h[2] * K, 31) ^ (h[2] >> 31);
@@ -77,19 +75,16 @@ REGISTER_FAMILY(ChibiHash,
    $.src_status = HashFamilyInfo::SRC_ACTIVE
  );
 
-REGISTER_HASH(ChibiHash64_V2,
-   $.desc            = "ChibiHash64 - Version 2",
+REGISTER_HASH(ChibiHash2,
+   $.desc            = "ChibiHash64, v2",
    $.hash_flags      =
          FLAG_HASH_ENDIAN_INDEPENDENT,
    $.impl_flags      =
-         FLAG_IMPL_CANONICAL_LE |
+         FLAG_IMPL_CANONICAL_LE          |
          FLAG_IMPL_LICENSE_PUBLIC_DOMAIN |
-         FLAG_IMPL_MULTIPLY_64_64 |
-         FLAG_IMPL_ROTATE |
-         0,
+         FLAG_IMPL_MULTIPLY_64_64        |
+         FLAG_IMPL_ROTATE,
    $.bits            = 64,
-   // not sure what's supposed to be done here since ChibiHash always reads in
-   // little-endian order ¯\_(ツ)_/¯
    $.verification_LE = 0x65ED889A,
    $.verification_BE = 0x37C9D593,
    $.hashfn_native   = ChibiHash64_V2<false>,
