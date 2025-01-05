c9436fea6c2f707554ea9a38e91b5487c12b8b0d
Frank J. T. Wojcik <fwojcik@uw.edu>
2025-01-04 19:25:25 -0800

    Fix misnamed variable in platform detection

--- platform/builtins.cmake
+++ platform/builtins.cmake
@@ -19,7 +19,7 @@ findVariant(LIKELY)
 set(EXPECTP_VARIANTS
   "expectp()"
   "expectp"
-  "@EXPECT_IMPL@\n"
+  "@LIKELY_IMPL@\n"
   3
 )
 # If we don't have useful likely()/unlikely() macros, then don't
