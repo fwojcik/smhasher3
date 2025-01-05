46fe17ad690f893d20edafa61f52c942008669ac
Frank J. T. Wojcik <fwojcik@uw.edu>
2025-01-04 19:51:48 -0800

    Fix monotonic_clock test code

--- platform/mclock_test.cpp
+++ platform/mclock_test.cpp
@@ -1,19 +1,26 @@
+
 #include <cstdio>
 
 #include "curvariant.h"
 
-volatile int state;
+// These are for the PRIu64 macro below
+#define __STDC_FORMAT_MACROS 1 // Some older gcc installations need this
+#include <cinttypes>
+
+volatile unsigned state;
 
 int main(void) {
-    int sum = 0;
+    unsigned sum = 0;
     uint64_t start, end;
 
+    state = 1;
+
     start = monotonic_clock();
-    for (int i = 1; i <= (1 << 24); i++) {
+    for (unsigned i = 1; i <= (1 << 24); i++) {
         sum += state * i;
     }
     end = monotonic_clock();
 
-    unsigned long delta = end - start;
-    printf("Sum:  %d\nTime taken: %ld\n", sum, delta);
+    uint64_t delta = end - start;
+    printf("Sum:  %u\nTime taken (ns): %" PRIu64 "\n", sum, delta);
 }
